e43169c353638ad5f93548e8599345139b10a0bd
B Krishna Chaitanya <bkchaitan94@gmail.com>
2014-11-07 14:30:31 +0530

    Block backend

--- src/_php/ajax.php
+++ src/_php/ajax.php
@@ -6,8 +6,16 @@ function approve($db, $id) {
 	if (!$st->execute()) return render_500();
 	return render_202();
 }
+
 function block($db, $id) {
 	$st = $db->prepare("UPDATE user SET blocked=1 WHERE id=?");
+	$st->bind_param('i', $_POST['id']);
+	if (!$st->execute()) return render_500();
+	return render_202();
+}
+
+function unblock($db, $id) {
+	$st = $db->prepare("UPDATE user SET blocked=0 WHERE id=?");
 	$st->bind_param('i', $_POST['id']);
 	if (!$st->execute()) return render_500();
 	return render_202();
@@ -30,6 +38,13 @@ else {
 			$current_user = SessionManager::getCurrentUser();
 			if ($current_user->type != 2) return render_401();
 			echo block($db, $_POST["id"]);
+			break;
+		case 'unblock':
+			if (!isset($_POST["id"])) return render_400();
+			if (!SessionManager::isLoggedin()) return render_401();
+			$current_user = SessionManager::getCurrentUser();
+			if ($current_user->type != 2) return render_401();
+			echo unblock($db, $_POST["id"]);
 			break;
 		default:
 			echo render_400();
