1961ef974efc6c4992005c42bcbca65e87827539
B Krishna Chaitanya <bkchaitan94@gmail.com>
2014-11-13 13:17:28 +0530

    Updated global functions

--- src/util/global_functions.php
+++ src/util/global_functions.php
@@ -1,34 +1,34 @@
 <?php
-function render_202() {
+function render_202($body='') {
 	// Accepted
 	http_response_code(202);
-	return '';
+	return $body;
 }
 
-function render_400() {
+function render_400($body='') {
 	// Bad request
 	http_response_code(400);
-	return '400';
+	return $body;
 }
-function render_401() {
+function render_401($body='') {
 	// Unauthorized
 	http_response_code(401);
-	return '401';
+	return $body;
 }
-function render_405() {
+function render_405($body='') {
 	// Method not allowed
 	http_response_code(405);
-	return '405';
+	return $body;
 }
-function render_404() {
+function render_404($body='') {
 	// Method not allowed
 	http_response_code(404);
-	return '404';
+	return $body;
 }
 
-function render_500() {
+function render_500($body='') {
 	// Server error
 	http_response_code(404);
-	return '404';
+	return $body;
 }
 ?>
